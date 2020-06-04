8bfec48c0166fd4207d08eccd0f7bac7c15f37f0
Emily Inns <nyssa.inns@gmail.com>
2020-06-04 23:32:54 +0200

    Candy "pickup" and sound implemented
    Candy currently gives no effect when picked up

---
+++ Assets/Scripts/Candy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candy : MonoBehaviour
+{
+    AudioSource audioSource;
+    [SerializeField] float soundVolume = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        
+        AudioSource.PlayClipAtPoint(audioSource.clip, Camera.main.transform.position, soundVolume);
+        Destroy(gameObject);
+    }
+}
