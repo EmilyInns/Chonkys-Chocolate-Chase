65a034be6372e1b14dd714fa3292e067258ad34b
Emily Inns <nyssa.inns@gmail.com>
2020-06-13 17:32:16 +0200

    Refactored level loading into its own script, fixed menu button bug
    Singleton code on object was causing the problem

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+        public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+}
